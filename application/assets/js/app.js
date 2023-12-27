7aa8fb2bc43bb7d1441c4162420c97d784f9506e
muhammadfaris <muhammadfarisadira5@gmail.com>
2023-12-27 17:12:24 +0700

    Launching Interface

--- application/assets/js/app.js
+++ application/assets/js/app.js
@@ -9,22 +9,22 @@
       ? (n("html").removeAttr("dir"),
         n("#dark-mode-switch").prop("checked", !1),
         n("#rtl-mode-switch").prop("checked", !1),
-        n("#bootstrap-style").attr("href", "http://localhost/Mbulan/application/assets/css/bootstrap.min.css"),
-        n("#app-style").attr("href", "http://localhost/Mbulan/application/assets/css/app.min.css"),
+        n("#bootstrap-style").attr("href", "assets/css/bootstrap.min.css"),
+        n("#app-style").attr("href", "assets/css/app.min.css"),
         sessionStorage.setItem("is_visited", "light-mode-switch"))
       : 1 == n("#dark-mode-switch").prop("checked") && "dark-mode-switch" === e
       ? (n("html").removeAttr("dir"),
         n("#light-mode-switch").prop("checked", !1),
         n("#rtl-mode-switch").prop("checked", !1),
-        n("#bootstrap-style").attr("href", "http://localhost/application/Mbulan/assets/css/bootstrap-dark.min.css"),
-        n("#app-style").attr("href", "http://localhost/Mbulan/application/assets/css/app-dark.min.css"),
+        n("#bootstrap-style").attr("href", "assets/css/bootstrap-dark.min.css"),
+        n("#app-style").attr("href", "assets/css/app-dark.min.css"),
         sessionStorage.setItem("is_visited", "dark-mode-switch"))
       : 1 == n("#rtl-mode-switch").prop("checked") &&
         "rtl-mode-switch" === e &&
         (n("#light-mode-switch").prop("checked", !1),
         n("#dark-mode-switch").prop("checked", !1),
-        n("#bootstrap-style").attr("href", "http://localhost/Mbulan/application/assets/css/bootstrap-rtl.min.css"),
-        n("#app-style").attr("href", "http://localhost/Mbulan/application/assets/css/app-rtl.min.css"),
+        n("#bootstrap-style").attr("href", "assets/css/bootstrap-rtl.min.css"),
+        n("#app-style").attr("href", "assets/css/app-rtl.min.css"),
         n("html").attr("dir", "rtl"),
         sessionStorage.setItem("is_visited", "rtl-mode-switch"));
   }
